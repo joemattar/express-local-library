cae76aa304a02281f8ac759e3a41991f1354ebd5
Joe M <joemattar@gmail.com>
2022-12-06 16:42:22 -0500

    Update mongoose pt

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ app.use(helmet());
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
-const mongoDB = process.env.MONGODB_URI;
+const dev_db_url =
+  "mongodb+srv://admin:ot1BNBmsKky7syOP@cluster0.2fcw6ot.mongodb.net/local_library?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
