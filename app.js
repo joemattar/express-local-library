d03341e337c29d3edbbf564f4b46b22ac861d833
Joe Mattar <joe@cadsharp.com>
2023-02-01 19:42:21 -0500

    Update mongo testing uri

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(helmet());
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
-const mongoDB = process.env.MONGODB_URI;
+const mongoDB = process.env.MONGODB_URI || process.env.MONGODB_URI_TESTING;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
